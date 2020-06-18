b14b29ed7ea04f6ab6fd1f6d16e47d0c18ac024f
swee lah <mountainfreezer@hotmail.com>
2020-06-18 15:12:18 +0800

    bug fixes

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
    name='pywin_env_shift',
-   version='0.1.0',
-   author='ps neo',
+   version='0.1.1',
+   author='psneo',
    license='LICENSE.txt',
    packages=['pywin_env_shift'],
    description='A package to help deploy your venv',
+   url='https://github.com/psneo/pywin_env_shift',
    install_requires=[],
 )
